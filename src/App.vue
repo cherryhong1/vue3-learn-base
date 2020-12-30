493f6b70da8ad96d5d2626020909d963eb0ccf80
爱怎么敷衍 <104436626h@gmail.com>
2020-12-30 14:27:54 +0800

    vue3中reactive,toRefs,computed,ref的基础使用

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,55 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>{{count}}</h1>
+  <h2>{{double}}</h2>
+  <h3 v-for="num in numberArr" :key="num">{{num}}</h3>
+  <h4>{{person.name}}</h4>
+  <button @click="increase">d(^^*)+1</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { computed,reactive,toRefs} from "vue";
+interface DateProps {
+  count: number;
+  double: number;
+  increase: () => void;
+  numberArr: number[];
+  person: {
+    name?: string;
+  };
+}
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  props:{
+    title: String,
+  },
+  setup(props) {
+    console.log(props.title)
+    // const count = ref(0)
+    // const increase = ()=> {
+    //   return  count.value++
+    // }
+    // const double = computed(()=>{
+    //  return count.value*2
+    // })
+    const countDate: DateProps = reactive({
+      count:0,
+      increase:()=>{
+        countDate.count++
+      },
+      double:computed(()=> {
+       return countDate.count*2
+      }),
+      numberArr:[1,2,3],
+      person: {}
+    })
+    countDate.numberArr[0]=10
+    countDate.person.name = "lisa"
+    const showCountData = toRefs(countDate)
+    return{
+      ...showCountData
+    }
   }
 });
 </script>
